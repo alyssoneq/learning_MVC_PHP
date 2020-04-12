58c193b1d121d36af4a53592dc004885af8e8b70
siralysson <siralyssongaming@gmail.com>
2020-04-12 16:35:37 -0300

    creating register and login views

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -3,7 +3,7 @@
 	class Users extends Controller {
 		public function __construct(){}
 		
-		// Method to load the form
+		// Method to load the register form 
 		// This also handles the data submission 
 		public function register(){
 			// Check for POST
@@ -25,6 +25,26 @@
 				];
 				// Load form view
 				$this->view('users/register',$data);
+			}
+		}
+		
+		// Method to load the login form 
+		public function login(){
+			// Check for POST
+			if($_SERVER['REQUEST_METHOD'] == 'POST'){
+				// Process the form
+			} else{
+				// Init data
+				// This is a quality of life practice
+				// Doing this, users dont have to retype everything again
+				$data = [
+					'email' => '',
+					'password' => '',
+					'email_err' => '',
+					'password_err' => ''
+				];
+				// Load form the login view
+				$this->view('users/login',$data);
 			}
 		}
 	}
